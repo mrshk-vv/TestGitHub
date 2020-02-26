9c700ef8890fad960098678b5363d753f553f709
mrshk-vv <777mrshk-vv@noreply.github.com>
2020-02-26 12:20:17 +0200

    New changes !

--- TestGitHub/Program.cs
+++ TestGitHub/Program.cs
@@ -7,6 +7,10 @@ namespace TestGitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Ruslan pidoras!");
+            for(int i = 0;i > 10; i++)
+            {
+                Console.WriteLine("Ruslan pidoras!");
+            }
         }
     }
 }
