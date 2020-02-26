51a1bccc1b828dfa478b342aa48bfe1dd517dd0b
vitalik23 <w.basanets2301@gmail.com>
2020-02-26 12:51:21 +0200

    hello

--- TestGitHub/Program.cs
+++ TestGitHub/Program.cs
@@ -7,6 +7,7 @@ namespace TestGitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Ruslan pidoras!");
+            Console.WriteLine("We all is gavno!");
             
         }
     }
